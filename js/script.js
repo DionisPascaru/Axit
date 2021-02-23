a79a650dac60df65fff09d43ff7367233c211913
unknown <denis.pascaru95@gmail.com>
2021-02-23 12:23:21 +0200

    23.02.2021 - responsive design

--- js/script.js
+++ js/script.js
@@ -2,4 +2,8 @@ $(document).ready(function(){
     $('.logo').on('click', function(){
         $("html").animate({ scrollTop: 0 }, 100);
     });
+
+    $('.collapse-btn').click(function(){
+        $('.nav-menu ul').toggleClass('open-menu');
+    });
 });
